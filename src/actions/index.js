1021187b7b9d1fb8644aff5ca1e44c90484458f5
Gian Felipe <gianfelipe93@gmail.com>
2018-02-10 16:04:03 +1100

    Creating request to weather forecast api

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axious from 'axios'
+
+const API_KEY = 'e9cb2f813741a0ca5b8e308d0d06e412'
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`
+    const request = axious.get(url)
+        
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
