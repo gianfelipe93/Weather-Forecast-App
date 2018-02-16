3956ddfb9226e00808867bc11012fbf09460ae46
Gian Felipe <gianfelipe93@gmail.com>
2018-02-17 10:58:27 +1100

    Calling weather forecast API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axious from 'axios'
 
 const API_KEY = 'e9cb2f813741a0ca5b8e308d0d06e412'
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
